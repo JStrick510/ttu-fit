1ac600438b7b07841f219d441db5dd6357d6bf18
JStrick510 <jacobstrickland510@gmail.com>
2021-04-21 10:54:04 -0500

    Added variables to the user class to store

--- TTUFit/TTUFit/User.cs
+++ TTUFit/TTUFit/User.cs
@@ -9,5 +9,29 @@ namespace TTUFit
         public String Username { get; set; }
 
         public String Password { get; set; }
+
+        public int age { get; set; }
+
+        public double goalWeight { get; set; }
+
+        public double currentWeight { get; set; }
+
+        public double heightInches { get; set; }
+
+        public int dailyCals { get; set; }
+
+        public enum Gender { Female, Male }
+        public Gender gender { get; set; }
+
+        public enum Activity {  Seditary, LightlyActive, ModeratelyActive, VeryActive, ExtermelyActive }
+        public Activity activity { get; set; }
+
+        public double weightChangeWeek { get; set; }
+
+        public double fatPercentage { get; set; }
+
+        public double carbPercentage { get; set; }
+
+        public double proteinPercentage { get; set; }
     }
 }
